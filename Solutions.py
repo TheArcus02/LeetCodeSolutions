0d5a58af1f0a44c1af9d643f881c1ac91780e4a6
TheArcus02 <m.urbaniak.inf@gmail.com>
2023-07-03 21:30:53 +0200

    added permutations solution

--- Solutions.py
+++ Solutions.py
@@ -30,12 +30,31 @@ class Solution:
                     return base[:i]
         return base
 
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) == 0 or len(nums) == 1:
+            return [nums]
+        if len(nums) == 2:
+            new_perm = nums.copy()
+            new_perm[0], new_perm[1] = new_perm[1], new_perm[0]
+            return [nums, new_perm]
+
+        permutations = []
+        for i in range(len(nums)):
+            print("-----------------pemutation-----------------")
+            print("i: ", i)
+            print(nums)
+            print(self.permute(nums[:i] + nums[i + 1 :]))
+            for perm in self.permute(nums[:i] + nums[i + 1 :]):
+                permutations.append([nums[i]] + perm)
+        return permutations
+
 
 def main():
     solution = Solution()
     # print(solution.twoSum([-1, -2, -4, -5], -9))
     # print(solution.romanToInt("MCMXCIV"))
-    print(solution.longestCommonPrefix(["reflower", "flow", "flight"]))
+    # print(solution.longestCommonPrefix(["reflower", "flow", "flight"]))
+    print(len(solution.permute([1, 2, 3, 4])))
 
 
 if __name__ == "__main__":
