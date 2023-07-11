1bb0e8cfa0b9470246317f1cfdf3d6b8f1588e09
TheArcus02 <m.urbaniak.inf@gmail.com>
2023-07-11 19:46:15 +0200

    maxDepth solution

---
+++ maxDepth.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        l = self.maxDepth(root.left)
+        r = self.maxDepth(root.right)
+
+        return max(l, r) + 1
+
+
+def main():
+    solution = Solution()
+    tree = TreeNode(3)
+    tree.left = TreeNode(9)
+    tree.right = TreeNode(20)
+    tree.right.left = TreeNode(15)
+    tree.right.right = TreeNode(7)
+
+    print(solution.maxDepth(tree))
+
+
+if __name__ == "__main__":
+    main()
